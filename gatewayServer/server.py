b99c481d2d12142ef40d392573a6f33e9f61f98a
devendra.dhande <devendra.dhande@sunbeaminfo.com>
2024-02-23 14:30:01 +0530

    LED status method is added in gateway server

--- gatewayServer/server.py
+++ gatewayServer/server.py
@@ -67,6 +67,33 @@ def ldr_methods():
     elif(request.method == 'DELETE'):
         pass
 
+@server.route("/led", methods = {'GET', 'POST', 'PUT', 'DELETE'})
+def led_methods():
+    if (request.method == 'GET'):
+        # to get data from database form a query
+        query = f"select * from sensorsLog;"
+
+        # execute query to get all temps from database
+        temps = db.execute_select_query(query)
+
+        # return temps into response
+        return response.create_response(temps)
+    elif(request.method == 'POST'):
+        # retrieve data from request body
+        value = request.get_json().get('value')
+        sen = request.get_json().get('led')
+        loc = "kaveri"
+
+        # insert data into database
+        query = f"insert into sensorsLog(location, sensor, value) values(\"{loc}\", \"{sen}\", {value});"
+        db.execute_query(query)
+
+        # return some response to the client
+        return response.create_response("LED Status is added successfully")
+    elif(request.method == 'PUT'):
+        pass
+    elif(request.method == 'DELETE'):
+        pass
 
 # run server
 server.run(host="0.0.0.0", port=4000, debug=True)
