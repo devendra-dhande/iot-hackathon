3cf7908ca2042cc4fa07e5a6014a7495227ae7d6
70303_Utkarsha <utkarshapnikam108@gmail.com>
2024-03-01 11:36:49 +0530

    nodeMCU1 LED toggle

--- nodeMCU1/nodeMCU1.ino
+++ nodeMCU1/nodeMCU1.ino
@@ -1,14 +1,13 @@
 
-#include <ESP8266WiFi.h>
+#include <stdio.h>
 
-const char* ssid = "SUNBEAM";
-const char* password = "1010101010";
+#define ssid "SUNBEAM"
+#define password "1010101010"
 
-
-const int switch1Pin = D0;
-const int switch2Pin = D1;
-const int led1Pin = D2;
-const int led2Pin = D3;
+#define switch1Pin D0
+#define switch2Pin D1
+#define led1Pin D2
+#define led2Pin D3
 
 
 int switch1State = 0;
@@ -17,32 +16,25 @@ int led1State = 0;
 int led2State = 0;
 
 void setup() {
-  Serial.begin(115200);
+  printf("Initializing...\n");
 
-
+  
   pinMode(switch1Pin, INPUT_PULLUP);
   pinMode(switch2Pin, INPUT_PULLUP);
 
-
+  
   pinMode(led1Pin, OUTPUT);
   pinMode(led2Pin, OUTPUT);
 
   
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connected to WiFi");
-
-
+}
 
 void loop() {
-  
+  // Read switch states
   switch1State = digitalRead(switch1Pin) == LOW;
   switch2State = digitalRead(switch2Pin) == LOW;
 
-  //for toggle LED states based on switch states
+  // Toggle LED states based on switch states
   if (switch1State != led1State) {
     led1State = !led1State;
     digitalWrite(led1Pin, led1State);
